4db9e0be49283e960349cb670b5d79150615125d
Harshvardhan Basava <harshvardhanbsv@gmail.com>
2020-05-30 22:53:48 +0530

    Navigation bar and search bar on home screen implemented.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:foodorder/scr/providers/app.dart';
-import 'package:foodorder/scr/providers/category.dart';
-import 'package:foodorder/scr/providers/product.dart';
-import 'package:foodorder/scr/providers/restaurant.dart';
-import 'package:foodorder/scr/providers/user.dart';
 import 'package:foodorder/scr/screens/home.dart';
-import 'package:foodorder/scr/screens/login.dart';
-import 'package:foodorder/scr/widgets/loading.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  runApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Food Order",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Home(),
+    );
+  }
 }
 
---
+++ lib/scr/screens/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Text('What would you like to eat?', style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),),
+                ),
+                Stack(
+                  children: <Widget>[
+                    IconButton(icon: Icon(Icons.notifications_none), onPressed: (){},),
+                    Positioned(
+                      top: 12,
+                      right: 12,
+                      child: Container(
+                        height: 10,
+                        width: 10,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 5,),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey[300],
+                      offset: Offset(1,1),
+                      blurRadius: 4,
+                    ),
+                  ]
+                ),
+                child: ListTile(
+                  leading: Icon(Icons.search, color: Colors.red,),
+                  title: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Find food and Restaurants.',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                  trailing: Icon(Icons.filter_list, color: Colors.red,),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
